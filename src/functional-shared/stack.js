6f536d0528bbecb0a2ab5c19bf00fded9b36d80a
John Zhang <john@zhang.io>
2015-02-05 11:39:08 -0800

    finished functional stack and queue

---
+++ src/functional-shared/stack.js
@@ -0,0 +1,27 @@
+var Stack = function() {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage:{
+      end:0
+    }
+  };
+  return someInstance
+};
+
+var stackMethods = {
+  push: function(value){
+  this.storage[this.storage.end++] = value;
+  },
+  pop: function(){
+    if(this.storage.end>0){
+      var value = this.storage[--this.storage.end];
+      delete this.storage[this.storage.end];
+      return value;
+    }
+  },
+  size: function(){
+    return this.storage.end;
+  }
+};
+
