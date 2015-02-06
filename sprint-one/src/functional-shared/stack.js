f1fb8a714e77dfec413ad0127612887dbf5944fa
John Zhang <john@zhang.io>
2015-02-06 11:05:45 -0800

    init commit for webstorm

---
+++ sprint-one/src/functional-shared/stack.js
@@ -0,0 +1,26 @@
+var Stack = function() {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    end:0
+  };
+  _.extend(someInstance, stackMethods);
+  return someInstance
+};
+
+var stackMethods = {
+  push: function(value){
+    this[this.end++] = value;
+  },
+  pop: function(){
+    if(this.end>0){
+      var value = this[--this.end];
+      delete this[this.end];
+      return value;
+    }
+  },
+  size: function(){
+    return this.end;
+  }
+};
+
