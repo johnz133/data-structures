4f78e2386200cadba8809e70261afbefcb1a1dcd
John Zhang <john@zhang.io>
2015-02-06 11:33:34 -0800

    webstorm test

---
+++ sprint-two/src/linkedList.js
@@ -0,0 +1,41 @@
+var LinkedList = function(){
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value) {
+    var newNode = Node(value);
+    if(list.head === null){
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
+  };
+
+
+  list.removeHead = function(){
+    var returnValue = list.head.value;
+    list.head = list.head.next;
+    return returnValue;
+  };
+
+  list.contains = function(target){
+  };
+
+  return list;
+};
+
+var Node = function(value){
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+
+  return node;
+};
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
